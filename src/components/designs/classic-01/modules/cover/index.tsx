f6b1ded795f4da83d33d5bf3711dde4c5a4708ac
Muhamad Danang Priambodo <218716308+mdanangp@users.noreply.github.com>
2025-08-02 09:30:29 +0700

    add guest name from query params

--- src/components/designs/classic-01/modules/cover/index.tsx
+++ src/components/designs/classic-01/modules/cover/index.tsx
@@ -1,8 +1,11 @@
 import { Data } from "@/components/module-form/cover";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export const Cover = ({ data }: { data: Data }) => {
+	const searchParams = useSearchParams();
+	const guestName = searchParams.get("to");
 	return (
 		<section className="text-center py-12 bg-white">
 			<h1 className="text-4xl font-bold">{data?.title ?? "Cover Title"}</h1>
@@ -15,6 +18,11 @@ export const Cover = ({ data }: { data: Data }) => {
 					width={400}
 					height={300}
 				/>
+			</div>
+			<div className="flex justify-center">
+				<p className="text-sm text-gray-500">Kepada Yth.</p>
+				{/* guest name from query params */}
+				<p className="text-sm text-gray-500">{guestName}</p>
 			</div>
 		</section>
 	);
